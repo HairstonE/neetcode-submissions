6983ab79e3564f667f3588aff82c587aaa9403ae
H <25857835+HairstonE@users.noreply.github.com>
2026-06-29 10:11:08 -0400

    Add: top-k-elements-in-list - submission-6

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-6.rs
@@ -0,0 +1,18 @@
+impl Solution {
+    pub fn top_k_frequent(nums: Vec<i32>, k: i32) -> Vec<i32> {
+        let mut counts = HashMap::new();
+        for num in nums {
+            *counts.entry(num).or_insert(1) += 1;
+        }
+        let mut heap:BinaryHeap<(i32, i32)> = counts.into_iter()
+        .map(|(k, v)| (v, k))
+        .collect();
+        let mut res = vec![];
+        for i in 0..k {
+            if let Some(largest) = heap.pop(){
+                res.push(largest.1);
+            }
+        }
+        res
+    }
+}
